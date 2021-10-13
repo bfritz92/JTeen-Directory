491a9bbf51ef18ce5e390bf632fb9162ab8e8d75
brian <fritz@jfmd.org>
2021-10-13 13:21:20 -0400

    add content to content-single

--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -14,8 +14,9 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<header class="entry-header alignwide">
+		
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-		<?php twenty_twenty_one_post_thumbnail(); ?>
+		
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
@@ -31,6 +32,33 @@
 			)
 		);
 		?>
+		<img class="resource--img" src="<?php the_field ('logo'); ?>">
+		<div class="tags">
+			<h4 class="tags--title"><?php the_field ('category'); ?></h4>
+			<h4 class="tags--title"><?php the_field ('time_of_year'); ?></h4>
+			<h4 class="tags--title"><?php the_field ('genders_served'); ?></h4>
+		</div>
+		<dl class="resource--info">
+			<div>
+				<dd><h4 class="resource--info--org"><?php the_field ('presenting_org'); ?></h4></dd>
+			</div>
+			<div>
+				<dt><p><strong class="resource--info--label">Ages/Grades Served:</strong> </p></dt>
+				<dd><p class="resource--info--entry"><?php the_field ('grades_served'); ?></p></dd>
+			</div>
+			<div>
+				<dt><p><strong> class="resource--info--label">Contact:</strong></p></dt>
+				<dd><p class="resource--info--entry"><?php the_field ('contact_name'); ?>, <?php the_field ('contact_email'); ?></p></dd>
+			</div>
+			<div>
+				<dt><p><strong class="resource--info--label">Contact:</strong></p></dt>
+				<dd><p class="resource--info--entry"><?php the_field ('contact_name'); ?>, <?php the_field ('contact_email'); ?></p></dd>
+			</div>
+		</dl>
+		<?php the_field ('content'); ?>
+		<div class="resource--register">
+			<a href="<?php the_field ('website'); ?>">Register Now</a>
+		</div>
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer default-max-width">
