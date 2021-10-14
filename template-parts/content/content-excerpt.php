382dd6d8e509d926a29f7336c13163a1733476f1
brian <fritz@jfmd.org>
2021-10-14 09:39:20 -0400

    testing

--- template-parts/content/content-excerpt.php
+++ template-parts/content/content-excerpt.php
@@ -14,12 +14,9 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<?php get_template_part( 'template-parts/header/excerpt-header', get_post_format() ); ?>
-
+	<h2>TESTING TITLES AND SHIT</h2>
 	<div class="entry-content">
 		<?php get_template_part( 'template-parts/excerpt/excerpt', get_post_format() ); ?>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer default-max-width">
-		<?php twenty_twenty_one_entry_meta_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-${ID} -->
