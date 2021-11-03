3bf8c328a823f87bf8dbefaa309253d44dadaa81
brian <fritz@jfmd.org>
2021-11-03 09:39:56 -0400

    rearrangements

--- search-filter/results.php
+++ search-filter/results.php
@@ -58,7 +58,11 @@ if ( $query->have_posts() )
 				<img class="resource--img" src="<?php the_field ('logo'); ?>">
 				<div class="resource--info">
 					<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-					
+					<div class="tags">
+						<h4 class="tags--title"><?php the_field ('category'); ?></h4>
+						<h4 class="tags--title"><?php the_field ('time_of_year'); ?></h4>
+						<h4 class="tags--title"><?php the_field ('genders_served'); ?></h4>
+					</div>
 					<dl class="resource--info--list">
 						<div  class="resource--info--org">
 							<dd><h4><?php the_field ('presenting_org'); ?></h4></dd>
@@ -76,11 +80,7 @@ if ( $query->have_posts() )
 							<dd><p><a href="<?php the_field ('website'); ?>"><?php the_field ('website'); ?></a></p></dd>
 						</div>
 					</dl>
-					<div class="tags">
-						<h4 class="tags--title"><?php the_field ('category'); ?></h4>
-						<h4 class="tags--title"><?php the_field ('time_of_year'); ?></h4>
-						<h4 class="tags--title"><?php the_field ('genders_served'); ?></h4>
-					</div>
+					
 					<p><br /><?php the_excerpt(); ?></p>
 					
 				</div>
