1db9920d0f0dd840870e1f1d8e7627730b999974
brian <fritz@jfmd.org>
2021-11-04 13:58:24 -0400

    WHERE ARE YOU

--- template-parts/content/content-none.php
+++ template-parts/content/content-none.php
@@ -12,7 +12,7 @@
 ?>
 
 <section class="no-results not-found">
-	<header class="page-header alignwide">
+	<header class="page-header alignfull">
 		<?php if ( is_search() ) : ?>
 
 			<h1 class="page-title">
