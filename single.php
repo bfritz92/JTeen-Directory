e54ff6e3400a3256b20fba0429b5cbba0c15e311
brian <fritz@jfmd.org>
2021-12-15 13:03:40 -0500

    ...remove post footer?

--- single.php
+++ single.php
@@ -17,20 +17,7 @@ while ( have_posts() ) :
 
 	get_template_part( 'template-parts/content/content-single' );
 
-	if ( is_attachment() ) {
-		// Parent post navigation.
-		the_post_navigation(
-			array(
-				/* translators: %s: Parent post link. */
-				'prev_text' => sprintf( __( '<span class="meta-nav">Published in</span><span class="post-title">%s</span>', 'twentytwentyone' ), '%title' ),
-			)
-		);
-	}
-
-	// If comments are open or there is at least one comment, load up the comment template.
-	if ( comments_open() || get_comments_number() ) {
-		comments_template();
-	}
+	
 
 endwhile; // End of the loop.
 
