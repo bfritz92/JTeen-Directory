53923e1974292d39465d973a86c7bdddfd5602f3
brian <fritz@jfmd.org>
2021-12-15 13:04:34 -0500

    no, for real tho.

--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -54,12 +54,6 @@
 		</div>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer default-max-width">
-		<?php twenty_twenty_one_entry_meta_footer(); ?>
-	</footer><!-- .entry-footer -->
 
-	<?php if ( ! is_singular( 'attachment' ) ) : ?>
-		<?php get_template_part( 'template-parts/post/author-bio' ); ?>
-	<?php endif; ?>
 
 </article><!-- #post-<?php the_ID(); ?> -->
